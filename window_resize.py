3e66ab6dd48fe7c459f93c8fe2deab1828d2f8ba
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2022-07-23 14:40:42 +0530

    Update window_resize.py

--- window_resize.py
+++ window_resize.py
@@ -11,26 +11,26 @@ from tkinter import *
 
 
 root = Tk()
-root.title("Window Resize")
-root.geometry("600x600")
+root.title("Window Resizer")
+root.geometry("300x200")
 
 def apply_getval():
     height= heightvalue.get()
     width = widthvalue.get()
     root.geometry(f"{width}x{height}")
 
-
-Label(root,text= "Width", font= "Lucida 13 bold").grid(row=0,column=1)
-Label(root, text = "Height", font= "Lucida 13 bold").grid(row=1,column=1)
+Label(root, text= "Window Resizer", font= "Lucida 19 bold", pady=20).grid(column=2)
+Label(root,text= "Width", font= "Lucida 13 bold").grid(row=1,column=1)
+Label(root, text = "Height", font= "Lucida 13 bold").grid(row=2,column=1)
 
 widthvalue = StringVar()
 heightvalue = StringVar()
 
-Entry(root, textvariable= widthvalue).grid(row=0 , column=2)
-Entry(root, textvariable= heightvalue).grid(row= 1, column=2)
+Entry(root, textvariable= widthvalue).grid(row=1 , column=2)
+Entry(root, textvariable= heightvalue).grid(row= 2, column=2)
 
 
 
-Button(text= "Apply" , command= apply_getval, font= "Lucida 10 bold").grid( column=2)
+Button(text= "Apply" , command= apply_getval, pady= 2, font= "Lucida 10 bold").grid( column=2)
 
 root.mainloop()
